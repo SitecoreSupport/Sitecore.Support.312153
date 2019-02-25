15846a4fbd9ba9184f8c33bb4dbab5dd3917d39d
DK\dmsa <dmsa@sitecore.net>
2019-02-25 10:38:11 +0200

    Origin code of the "RegularExpressionValidation" class.

---
+++ src/Sitecore.Support.312153/ExperienceForms/Mvc/Models/Validation/RegularExpressionValidation.cs
@@ -0,0 +1,69 @@
+﻿using Sitecore.ExperienceForms.Mvc.Models.Validation;
+using Sitecore.ExperienceForms.Mvc.Models.Validation.Parameters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Sitecore.Support.ExperienceForms.Mvc.Models.Validation
+{
+  public class RegularExpressionValidation : ValidationElement<RegularExpressionParameters>
+  {
+    public RegularExpressionValidation(ValidationDataModel validationItem)
+        : base(validationItem)
+    {
+    }
+
+    protected virtual string RegularExpression { get; set; }
+
+    protected virtual string Title { get; set; }
+
+    public override IEnumerable<ModelClientValidationRule> ClientValidationRules
+    {
+      get
+      {
+        if (string.IsNullOrEmpty(RegularExpression))
+        {
+          yield break;
+        }
+
+        var regex = new Regex(RegularExpression, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+
+        yield return new ModelClientValidationRegexRule(FormatMessage(Title), regex.ToString());
+      }
+    }
+
+    public override void Initialize(object validationModel)
+    {
+      base.Initialize(validationModel);
+
+      var obj = validationModel as StringInputViewModel;
+      if (obj != null)
+      {
+        Title = obj.Title;
+      }
+
+      RegularExpression = Parameters?.RegularExpression ?? string.Empty;
+    }
+
+    public override ValidationResult Validate(object value)
+    {
+      if (value == null || string.IsNullOrEmpty(RegularExpression))
+      {
+        return ValidationResult.Success;
+      }
+
+      var regex = new Regex(RegularExpression, RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
+
+      var stringValue = (string)value;
+      if (string.IsNullOrEmpty(stringValue) || regex.IsMatch(stringValue))
+      {
+        return ValidationResult.Success;
+      }
+
+      return new ValidationResult(FormatMessage(Title));
+    }
+  }
+}
